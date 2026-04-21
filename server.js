3afb2e5f74718316e0ae59ec73eb83ef8dfb64de
Anikate Kanoo <anikate955@gmail.com>
2026-04-21 17:14:08 +0530

    add frontend url

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://portfolio-frontend-theta-flame.vercel.app/" 
+}));
 app.use(express.json());
 
 app.post("/api/contact", async (req, res) => {
@@ -18,24 +20,24 @@ app.post("/api/contact", async (req, res) => {
 
     try {
         const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS,
-        },
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASS,
+            },
         });
 
-    await transporter.sendMail({
-        from: email,
-        to: process.env.EMAIL,
-        subject: `Portfolio Message from ${name}`,
-        text: message,
-    });
+        await transporter.sendMail({
+            from: email,
+            to: process.env.EMAIL,
+            subject: `Portfolio Message from ${name}`,
+            text: message,
+        });
 
-    res.status(200).json({ msg: "Message sent" });
-        } catch (err) {
-    res.status(500).json({ msg: "Error sending message" });
-        }
-    });
+        res.status(200).json({ msg: "Message sent" });
+    } catch (err) {
+        res.status(500).json({ msg: "Error sending message" });
+    }
+});
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running"));
